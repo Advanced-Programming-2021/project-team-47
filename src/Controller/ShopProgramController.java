7378803793a28d15c313118c161052386871ad79
HamidRezaSaad <hr.m.szh@gmail.com>
2021-04-20 16:04:58 +0430

    Complete ShopMenu class

--- src/Controller/ShopProgramController.java
+++ src/Controller/ShopProgramController.java
@@ -3,8 +3,9 @@ package Controller;
 import Model.Cards;
 import Model.Players;
 
+
 public class ShopProgramController {
-    public boolean checkEnoughMoney(String username, String cardName) {
+    public static boolean checkEnoughMoney(String username, String cardName) {
         return Players.getPlayerByUsername(username).getMoney() >= Cards.getCardByName(cardName).getPrice();
     }
 }

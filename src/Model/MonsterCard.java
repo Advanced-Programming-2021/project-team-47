a9d4ff43e365082aba886112e4f9c1845e3502aa
HamidReza Saad <79264777+HamidRezaSaad@users.noreply.github.com>
2021-04-22 13:00:28 +0430

    Correction files

--- src/Model/MonsterCard.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public class MonsterCard extends Cards {
-
-    public MonsterCard(String cardName, int level, String type, int ATK, int DEF, String description, int price, Style style) {
-        super(cardName, level, type, ATK, DEF, description, price, style);
-    }
-}
--- src/Model/TrapCard.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class TrapCard extends Cards {
-    public TrapCard(String cardName, int level, String type, int ATK, int DEF, String description, int price, Style style) {
-        super(cardName, level, type, ATK, DEF, description, price, style);
-    }
-}

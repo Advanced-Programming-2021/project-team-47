5ba1148bce75c36aeb8ad5b622e3e1901bcaa647
HamidReza Saad <79264777+HamidRezaSaad@users.noreply.github.com>
2021-04-22 12:48:59 +0430

    Delete wrong folder

--- AP_Project/src/main/java/Model/SpellCard.java
+++
@@ -1,7 +0,0 @@
-package Model;
-
-public class SpellCard extends Cards {
-    public SpellCard(String cardName, int level, String type, int ATK, int DEF, String description, int price, Style style) {
-        super(cardName, level, type, ATK, DEF, description, price, style);
-    }
-}

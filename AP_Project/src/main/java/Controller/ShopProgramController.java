1a6d2cbe551f7a54e62ac38193f565fae7039ce2
HamidReza Saad <79264777+HamidRezaSaad@users.noreply.github.com>
2021-04-22 12:46:16 +0430

    Add Maven

---
+++ AP_Project/src/main/java/Controller/ShopProgramController.java
@@ -0,0 +1,30 @@
+package Controller;
+
+import Model.Cards;
+import Model.Players;
+
+import java.util.TreeMap;
+
+
+public class ShopProgramController {
+    private static ShopProgramController shopProgramController;
+
+    public static ShopProgramController getInstance() {
+        if (shopProgramController == null) {
+            shopProgramController = new ShopProgramController();
+        }
+        return shopProgramController;
+    }
+
+    public boolean checkEnoughMoney(String username, String cardName) {
+        return Players.getPlayerByUsername(username).getMoney() >= Cards.getCardByName(cardName).getPrice();
+    }
+
+    public TreeMap<String, Integer> sortCard() {
+        TreeMap<String, Integer> sortCard = new TreeMap<>();
+        for (Cards card : Cards.allCards) {
+            sortCard.put(card.getCardName(), card.getPrice());
+        }
+        return sortCard;
+    }
+}

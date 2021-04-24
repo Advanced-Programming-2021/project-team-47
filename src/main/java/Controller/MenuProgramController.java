e10952bf0d036dca055d3b1c8cef72f2f9574c57
masihnajafi02 <najafim2002@gmail.com>
2021-04-24 04:48:01 +0430

    debug menu detection

--- src/main/java/Controller/MenuProgramController.java
+++ src/main/java/Controller/MenuProgramController.java
@@ -17,17 +17,17 @@ public class MenuProgramController {
     public void run() {
         while (currentMenu != Menus.EXIT) {
             String command = GameProgramController.scanner.nextLine();
-            if (currentMenu == Menus.MAIN_MENU) {
+            if (currentMenu.equals(Menus.MAIN_MENU)) {
                 MainMenu.getInstance().run(command);
-            } else if (currentMenu == Menus.IMPORT_OR_EXPORT_MENU) {
+            } else if (currentMenu.equals(Menus.IMPORT_OR_EXPORT_MENU)) {
                 ImportOrExportMenu.getInstance().run(command);
-            } else if (currentMenu == Menus.LOGIN_MENU) {
+            } else if (currentMenu.equals(Menus.LOGIN_MENU)) {
                 LoginMenu.getInstance().run(command);
-            } else if (currentMenu == Menus.PROFILE_MENU) {
+            } else if (currentMenu.equals(Menus.PROFILE_MENU)) {
                 ProfileMenu.getInstance().run(command);
-            } else if (currentMenu == Menus.SCOREBOARD_MENU) {
+            } else if (currentMenu.equals(Menus.SCOREBOARD_MENU)) {
                 ScoreboardMenu.getInstance().run(command);
-            } else if (currentMenu == Menus.SHOP_MENU) {
+            } else if (currentMenu.equals(Menus.SHOP_MENU)) {
                 ShopMenu.getInstance().run(command);
             }
         }

b8f3ed2c0c3b68349eab18c8df8ed9e66e8e99d8
abolfazl <eshagh.abolfazl@gmail.com>
2021-05-05 13:40:03 -0700

    (duelMenu+ProgramController) update

--- src/main/java/View/DuelMenu.java
+++ src/main/java/View/DuelMenu.java
@@ -3,6 +3,9 @@ package View;
 import Controller.GameProgramController;
 import Model.Players;
 
+import Controller.GameProgramController;
+import Model.Players;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.function.Consumer;

7ce2b655e715d8b14253c8569c5d157ea9885bc2
abolfazl <eshagh.abolfazl@gmail.com>
2021-05-08 06:48:07 -0700

    CardTypes adjustments

--- src/main/java/Model/MonsterCard.java
+++ src/main/java/Model/MonsterCard.java
@@ -1,8 +1,12 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class MonsterCard extends Cards {
 
     public MonsterCard(String cardName, int level, String type, int ATK, int DEF, String description, int price, Style style) {
         super(cardName, level, type, ATK, DEF, description, price, style);
+        allMonsterCards.add(this);
     }
+    private ArrayList<MonsterCard> allMonsterCards=new ArrayList<>();
 }
--- src/main/java/Model/TrapCard.java
+++ src/main/java/Model/TrapCard.java
@@ -1,7 +1,12 @@
 package Model;
 
+import java.util.ArrayList;
+
 public class TrapCard extends Cards {
     public TrapCard(String cardName, int level, String type, int ATK, int DEF, String description, int price, Style style) {
         super(cardName, level, type, ATK, DEF, description, price, style);
+        allTrapCards.add(this);
     }
+    private ArrayList<TrapCard> allTrapCards=new ArrayList<>();
+
 }

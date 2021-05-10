eb7471cbcef6907a07f388b2b56b6dbefafcc78c
HamidReza Saad <79264777+HamidRezaSaad@users.noreply.github.com>
2021-05-10 17:27:39 +0430

    Add method to DuelMenu

--- src/main/java/View/DuelMenu.java
+++ src/main/java/View/DuelMenu.java
@@ -1,8 +1,12 @@
-package main.java.View;
+package View;
 
-import Model.Menus;
+import Controller.GameProgramController;
+import Controller.Regex;
+import Model.Deck;
+import Model.MainDeck;
+import Model.Players;
+import Model.SideDeck;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
@@ -10,7 +14,7 @@ import java.util.regex.Pattern;
 
 public class DuelMenu implements Runnable {
     public static HashMap<Pattern, Consumer<Matcher>> commandMap = new HashMap<>();
-    private static DuelMenu duelMenu;
+    public static DuelMenu duelMenu;
     private String firstPlayer;
     private String secondPlayer;
     private String phaseName;
@@ -27,7 +31,12 @@ public class DuelMenu implements Runnable {
     }
 
     public void run(String command) {
-
+        commandMap.put(Regex.DIRECT_ATTACK.label, DuelMenu.commandChecker::directAttack);
+        commandMap.put(Regex.ACTIVE_EFFECT.label, DuelMenu.commandChecker::activeEffect);
+        commandMap.put(Regex.SET.label, DuelMenu.commandChecker::set);
+        commandMap.put(Regex.SHOW_GRAVEYARD.label, DuelMenu.commandChecker::showGraveyard);
+        commandMap.put(Regex.DUEL_PLAYER.label, DuelMenu.commandChecker::startTwoPlayerDuel);
+        commandMap.put(Regex.DUEL_AI.label, DuelMenu.commandChecker::startTwoPlayerDuel);
     }
 
     public void takeCommand(String command) {
@@ -37,6 +46,28 @@ public class DuelMenu implements Runnable {
                 return;
             }
         System.out.println("invalid command");
+    }
+
+    public void checkErrorsGame(String username, int rounds) {
+        MainDeck playerMainDeck = (MainDeck) Players.getPlayerByUsername(username).getActiveDeck().get(0);
+        if (Players.getPlayerByUsername(username) == null) {
+            System.out.println(Response.usernameNotExist);
+        } else if (Players.getPlayerByUsername(username).getActiveDeck() == null) {
+            System.out.println(username + " " + Response.activeDeckNotAvailable);
+        } else if (!Players.getPlayerByUsername(username).isActiveDeckValid(Players.getPlayerByUsername(username))) {
+            System.out.println(username + "'s " + Response.invalidDeck);
+        } else if (!playerMainDeck.isDeckValid(playerMainDeck)) {
+            System.out.println(username + "'s " + Response.invalidDeck);
+        } else if (rounds != 3 && rounds != 1) {
+            System.out.println(Response.invalidRoundNumber);
+            return;
+        }
+        if (rounds == 3) {
+            SideDeck playerSideDeck = (SideDeck) Players.getPlayerByUsername(username).getActiveDeck().get(1);
+            if (!playerSideDeck.isDeckValid(playerSideDeck)) {
+                System.out.println(username + "'s " + Response.invalidDeck);
+            }
+        }
     }
 
     public String getFirstPlayer() {
@@ -119,5 +150,70 @@ public class DuelMenu implements Runnable {
 
     public void setCardAddressNumberSelected(int cardAddressNumberSelected) {
         this.cardAddressNumberSelected = cardAddressNumberSelected;
+    }
+
+    public void gameBoardPrint(String username) {
+        System.out.println(Players.getPlayerByUsername(username).getNickname() + ":" + Players.getPlayerByUsername(username).getLifePoint());
+        System.out.println("    ");
+        for (int i = 0; i < Players.getPlayerByUsername(username).getAllCardsInHandsArray().size(); ++i) {
+            System.out.print(Players.getPlayerByUsername(username).getCardsInHand(i));
+            System.out.println("    ");
+        }
+        System.out.println();
+        System.out.println(Deck.getDeckByOwner(username).getAllCardsNumber());
+        System.out.println("    ");
+        for (int z = 0; z < Players.getPlayerByUsername(username).getMonsterCardZoneArray().length; ++z) {
+            System.out.print(Players.getPlayerByUsername(username).getMonsterCardZone(z));
+            System.out.println("    ");
+        }
+        for (int j = 0; j < Players.getPlayerByUsername(username).getSpellCardZone().length; ++j) {
+            System.out.print(Players.getPlayerByUsername(username).getSpellCardZoneByCoordinate(j));
+            System.out.println("    ");
+        }
+        String fieldZone;
+        if (Players.getPlayerByUsername(username).getFieldZone().size() == 0) {
+            fieldZone = "E";
+        } else {
+            fieldZone = "O";
+        }
+        System.out.println(Players.getPlayerByUsername(username).getCardsInGraveyard().size() + "                      " + fieldZone);
+    }
+
+    public void gameBoard(String firstPlayer, String secondPlayer) {
+        gameBoardPrint(firstPlayer);
+        System.out.println("--------------------------");
+        gameBoardPrint(secondPlayer);
+    }
+
+    static class commandChecker {
+        static void directAttack(Matcher matcher) {
+
+        }
+
+        static void activeEffect(Matcher matcher) {
+
+        }
+
+        static void set(Matcher matcher) {
+
+        }
+
+        static void showGraveyard(Matcher matcher) {
+
+        }
+
+        static void startTwoPlayerDuel(Matcher matcher) {
+            String firstPlayerUsername = DuelMenu.getInstance().firstPlayer;
+            String secondPlayerUsername = DuelMenu.getInstance().secondPlayer;
+            int rounds = DuelMenu.getInstance().round;
+            if (!firstPlayerUsername.equals("ai") || !secondPlayerUsername.equals("ai")) {
+                DuelMenu.getInstance().checkErrorsGame(firstPlayerUsername, rounds);
+                DuelMenu.getInstance().checkErrorsGame(secondPlayerUsername, rounds);
+                GameProgramController.getInstance().startMultiplePlayerGame(matcher);
+            } else {
+                DuelMenu.getInstance().checkErrorsGame(firstPlayerUsername, rounds);
+                GameProgramController.getInstance().startMultiplePlayerGame(matcher);
+            }
+        }
     }
 }

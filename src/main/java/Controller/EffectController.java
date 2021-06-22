3e4b35060aed4107af529f2c8fc0bf6266014f29
HamidReza Saad <79264777+HamidRezaSaad@users.noreply.github.com>
2021-06-22 19:33:31 +0430

    Active Effect takeCommand Method

--- src/main/java/Controller/EffectController.java
+++ src/main/java/Controller/EffectController.java
@@ -8,27 +8,87 @@ import View.DuelMenu;
 import View.LoginMenu;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EffectController {
-    public void heraldOfCreation() {
+    public static HashMap<Pattern, Consumer<Matcher>> effectMap = new HashMap<>();
+    private static EffectController effectController;
+
+    public static EffectController getInstance() {
+        if (effectController == null) {
+            effectController = new EffectController();
+        }
+        return effectController;
+    }
+
+    static {
+        effectMap.put(Regex.HERALD_OF_CREATION.label, EffectController::heraldOfCreation);
+        effectMap.put(Regex.EXPLODER_DOGON.label, EffectController::exploderDagon);
+        effectMap.put(Regex.TEEEAFORMING.label, EffectController::terraforming);
+        effectMap.put(Regex.TERRATIGER.label, EffectController::terratiger);
+        effectMap.put(Regex.THE_TRICKY.label, EffectController::theTricky);
+        effectMap.put(Regex.RAIGEKI.label, EffectController::raigeki);
+        effectMap.put(Regex.CHANGE_OF_HEART.label, EffectController::changeOfHeart);
+        effectMap.put(Regex.HARPIES_FEATHER_DUSTER.label, EffectController::harpiesFeatherDuster);
+        effectMap.put(Regex.MYSTICAL_SPACE_TYPHOON.label, EffectController::mysticalSpaceTyphoon);
+        effectMap.put(Regex.TWIN_TWISTERS.label, EffectController::twinTwisters);
+        effectMap.put(Regex.SUPPLY_SQUAD.label, EffectController::supplySquad);
+        effectMap.put(Regex.POT_OF_GREED.label, EffectController::potOfGreed);
+        effectMap.put(Regex.MONSTER_REBORN.label, EffectController::monsterReborn);
+        effectMap.put(Regex.YAMI.label, EffectController::yami);
+        effectMap.put(Regex.FOREST.label, EffectController::forest);
+        effectMap.put(Regex.CLOSED_FOREST.label, EffectController::closedForest);
+        effectMap.put(Regex.UMIIRUKA.label, EffectController::umiiruka);
+        effectMap.put(Regex.SWORD_OF_DARK_DESTRUCTION.label, EffectController::swordOfDarkDestruction);
+        effectMap.put(Regex.BLACK_PENDANT.label, EffectController::blackPendant);
+        effectMap.put(Regex.UNITED_WE_STAND.label, EffectController::unitedWeStand);
+        effectMap.put(Regex.MAGNUM_SHIELD.label, EffectController::magnumShield);
+        effectMap.put(Regex.MIRROR_FORCE.label, EffectController::mirrorForce);
+        effectMap.put(Regex.ADVANCED_RITUAL_ART.label, EffectController::advancedRitualArt);
+        effectMap.put(Regex.MAGIC_CYLINDER.label, EffectController::magicCylinder);
+        effectMap.put(Regex.MIND_CRUSH.label, EffectController::mindCrush);
+        effectMap.put(Regex.SOLEMN_WARNING.label, EffectController::solemnWarning);
+        effectMap.put(Regex.CALL_OF_THE_HAUNTED.label, EffectController::callOfTheHaunted);
+        effectMap.put(Regex.COMMAND_KNIGHT.label, EffectController::commandKnight);
+        effectMap.put(Regex.YOMI_SHIP.label, EffectController::yomiShip);
+        effectMap.put(Regex.SUIJIN.label, EffectController::suijin);
+        effectMap.put(Regex.CRAB_TURTLE.label, EffectController::crabTurtle);
+        effectMap.put(Regex.SKULL_GUARDIAN.label, EffectController::skullGuardian);
+        effectMap.put(Regex.MAN_EATER_BUG.label, EffectController::manEaterBug);
+        effectMap.put(Regex.GATE_GUARDIAN.label, EffectController::gateGuardian);
+        effectMap.put(Regex.SCANNER_CARD.label, EffectController::scannerCard);
+    }
+
+    public void takeCommand(String command) {
+        for (Pattern commandReg : effectMap.keySet())
+            if (command.matches(commandReg.pattern())) {
+                effectMap.get(commandReg).accept(commandReg.matcher(command));
+                return;
+            }
+    }
+
+    public static void heraldOfCreation(Matcher matcher) {
         DuelMenu.getInstance().getCardsInHand().remove(0);
         DuelMenu.getInstance().setCardsInHand(LoginMenu.loginUsername);
     }
 
-    public void exploderDagon() {
+    public static void exploderDagon(Matcher matcher) {
         DuelMenu.getInstance().setSelectCard(null, 0);
     }
 
-    public void terratiger() {
+    public static void terratiger(Matcher matcher) {
         GameProgramController.getInstance().summon(LoginMenu.loginUsername.getUsername());
     }
 
-    public void theTricky() {
+    public static void theTricky(Matcher matcher) {
         DuelMenu.getInstance().setCardsInHand(DuelMenu.duelMenu.getFirstPlayer());
         GameProgramController.getInstance().summon(DuelMenu.duelMenu.getFirstPlayer().getUsername());
     }
 
-    public void terraforming() {
+    public static void terraforming(Matcher matcher) {
         for (int i = 0; i < Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).getFieldZone().size(); ++i) {
             if (Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).getFieldZone().get(i).getKind().equals("Field Spell")) {
                 Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).setCardsInHand(Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).getFieldZone().get(i), 0);
@@ -37,43 +97,43 @@ public class EffectController {
         }
     }
 
-    public void raigeki() {
+    public static void raigeki(Matcher matcher) {
         Players.getPlayerByUsername(DuelMenu.duelMenu.getSecondPlayer().getUsername()).getMonsterCardZoneArray().clear();
     }
 
-    public void changeOfHeart() {
+    public static void changeOfHeart(Matcher matcher) {
         Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).setMonsterCardZone(Players.getPlayerByUsername(DuelMenu.duelMenu.getSecondPlayer().getUsername()).getMonsterCardZone(0), 0);
     }
 
-    public void harpiesFeatherDuster() {
+    public static void harpiesFeatherDuster(Matcher matcher) {
         Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).getSpellCardZone().clear();
     }
 
-    public void mysticalSpaceTyphoon() {
+    public static void mysticalSpaceTyphoon(Matcher matcher) {
         Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).getSpellCardZone().remove(0);
     }
 
-    public void twinTwisters() {
+    public static void twinTwisters(Matcher matcher) {
         Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).setCardsInHand(null, 0);
         Players.getPlayerByUsername(DuelMenu.duelMenu.getSecondPlayer().getUsername()).getSpellCardZone().remove(0);
         Players.getPlayerByUsername(DuelMenu.duelMenu.getSecondPlayer().getUsername()).getSpellCardZone().remove(1);
     }
 
-    public void supplySquad() {
+    public static void supplySquad(Matcher matcher) {
         Deck.getDeckByOwner(DuelMenu.duelMenu.getFirstPlayer()).setCardsInDecks(Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).getMainDecks().getMainDeckCards().get(0).getCardName());
     }
 
-    public void potOfGreed() {
+    public static void potOfGreed(Matcher matcher) {
         Deck.getDeckByOwner(DuelMenu.duelMenu.getFirstPlayer()).setCardsInDecks(Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).getMainDecks().getMainDeckCards().get(0).getCardName());
         Deck.getDeckByOwner(DuelMenu.duelMenu.getFirstPlayer()).setCardsInDecks(Players.getPlayerByUsername(DuelMenu.duelMenu.getFirstPlayer().getUsername()).getMainDecks().getMainDeckCards().get(1).getCardName());
     }
 
-    public void monsterReborn() {
+    public static void monsterReborn(Matcher matcher) {
         GameProgramController.getInstance().summon(LoginMenu.loginUsername.getUsername());
     }
 
-    public void yami(String opponent) {
-        for (Cards card : Players.getPlayerByUsername(opponent).getMonsterCardZoneArray()) {
+    public static void yami(Matcher opponent) {
+        for (Cards card : Players.getPlayerByUsername(opponent.group(1)).getMonsterCardZoneArray()) {
             if (card.getKind().equals("Fiend") || card.getKind().equals("Fiend")) {
                 card.increaseATK(200);
                 card.increaseDEF(200);
@@ -85,8 +145,8 @@ public class EffectController {
         }
     }
 
-    public void forest(String opponent) {
-        for (Cards card : Players.getPlayerByUsername(opponent).getMonsterCardZoneArray()) {
+    public static void forest(Matcher opponent) {
+        for (Cards card : Players.getPlayerByUsername(opponent.group(1)).getMonsterCardZoneArray()) {
             if (card.getKind().equals("Insect") || card.getKind().equals("Beast") || card.getKind().equals("Beast-Warrior")) {
                 card.increaseATK(200);
                 card.increaseDEF(200);
@@ -94,15 +154,15 @@ public class EffectController {
         }
     }
 
-    public void closedForest(String turn) {
-        for (Cards card : Players.getPlayerByUsername(turn).getCardsInGraveyard()) {
+    public static void closedForest(Matcher turn) {
+        for (Cards card : Players.getPlayerByUsername(turn.group(1)).getCardsInGraveyard()) {
             if (card.getKind().equals("Insect") || card.getKind().equals("Beast") || card.getKind().equals("Beast-Warrior")) {
                 card.increaseATK(100);
             }
         }
     }
 
-    public void umiiruka() {
+    public static void umiiruka(Matcher matcher) {
         for (Cards card : Cards.allCards) {
             if (card.getKind().equals("Aqua")) {
                 card.increaseATK(500);
@@ -111,128 +171,127 @@ public class EffectController {
         }
     }
 
-    public void swordOfDarkDestruction(Cards cards) {
-        if (cards.getKind().equals("Aqua")) {
-            cards.decreaseDEF(400);
-            cards.increaseATK(500);
+    public static void swordOfDarkDestruction(Matcher cards) {
+        if (Cards.getCardByName(cards.group(1)).getKind().equals("Aqua")) {
+            Cards.getCardByName(cards.group(1)).decreaseDEF(400);
+            Cards.getCardByName(cards.group(1)).increaseATK(500);
         }
     }
 
-    public void blackPendant(Cards cards) {
-        cards.increaseATK(500);
+    public static void blackPendant(Matcher cards) {
+        Cards.getCardByName(cards.group(1)).increaseATK(500);
     }
 
-    public void unitedWeStand(Cards cards) {
-        cards.increaseATK(800);
+    public static void unitedWeStand(Matcher cards) {
+        Cards.getCardByName(cards.group(1)).increaseATK(800);
     }
 
-    public void magnumShield(String turn) {
-        for (Cards card : Players.getPlayerByUsername(turn).getMonsterCardZoneArray()) {
-            if (Players.getPlayerByUsername(turn).getMonsterZone().get(card).equals("OO")) {
+    public static void magnumShield(Matcher turn) {
+        for (Cards card : Players.getPlayerByUsername(turn.group(1)).getMonsterCardZoneArray()) {
+            if (Players.getPlayerByUsername(turn.group(1)).getMonsterZone().get(card).equals("OO")) {
                 card.increaseATK(card.getDEF());
             }
-            if (Players.getPlayerByUsername(turn).getMonsterZone().get(card).equals("DH")) {
+            if (Players.getPlayerByUsername(turn.group(1)).getMonsterZone().get(card).equals("DH")) {
                 card.increaseDEF(card.getATK());
             }
 
         }
     }
 
-    public void advancedRitualArt(String turn, String position, Cards cardName) {
-        GameProgramController.getInstance().ritualSummon(turn, position);
-        Players.getPlayerByUsername(turn).setCardsInGraveyard(cardName);
+    public static void advancedRitualArt(Matcher matcher) {//group1 -> turn / group2 -> Position / group3 -> Card
+        GameProgramController.getInstance().ritualSummon(matcher.group(1), matcher.group(2));
+        Players.getPlayerByUsername(matcher.group(1)).setCardsInGraveyard(Cards.getCardByName(matcher.group(3)));
     }
 
-    public void magicCylinder(Cards cards, String opponent) {
-        Players.getPlayerByUsername(opponent).decreaseLifePoint(cards.getATK());
+    public static void magicCylinder(Matcher matcher) {//group1 -> Opponent / group2 -> Card
+        Players.getPlayerByUsername(matcher.group(1)).decreaseLifePoint(Cards.getCardByName(matcher.group(2)).getATK());
     }
 
-    public void mirrorForce(String opponent) {
-        for (Cards card : Players.getPlayerByUsername(opponent).getMonsterCardZoneArray()) {
-            if (Players.getPlayerByUsername(opponent).getMonsterZone().get(card).equals("OO")) {
-                Players.getPlayerByUsername(opponent).removeFromArrayList(Players.getPlayerByUsername(opponent).getMonsterCardZoneArray(), Players.getPlayerByUsername(opponent).getMonsterZone(), card);
-                Players.getPlayerByUsername(opponent).setCardsInGraveyard(card);
+    public static void mirrorForce(Matcher opponent) {
+        for (Cards card : Players.getPlayerByUsername(opponent.group(1)).getMonsterCardZoneArray()) {
+            if (Players.getPlayerByUsername(opponent.group(1)).getMonsterZone().get(card).equals("OO")) {
+                Players.getPlayerByUsername(opponent.group(1)).removeFromArrayList(Players.getPlayerByUsername(opponent.group(1)).getMonsterCardZoneArray(), Players.getPlayerByUsername(opponent.group(1)).getMonsterZone(), card);
+                Players.getPlayerByUsername(opponent.group(1)).setCardsInGraveyard(card);
             }
         }
     }
 
-    public void mindCrush(String opponent, Cards cards) {
+    public static void mindCrush(Matcher matcher) {//group1 -> Opponent / group2 -> Card
         int done = 0;
-        for (Cards card : Players.getPlayerByUsername(opponent).getMonsterCardZoneArray()) {
-            if (card.equals(cards)) {
-                Players.getPlayerByUsername(opponent).removeFromArrayList(Players.getPlayerByUsername(opponent).getMonsterCardZoneArray(), Players.getPlayerByUsername(opponent).getMonsterZone(), cards);
+        for (Cards card : Players.getPlayerByUsername(matcher.group(1)).getMonsterCardZoneArray()) {
+            if (card.equals(Cards.getCardByName(matcher.group(2)))) {
+                Players.getPlayerByUsername(matcher.group(1)).removeFromArrayList(Players.getPlayerByUsername(matcher.group(1)).getMonsterCardZoneArray(), Players.getPlayerByUsername(matcher.group(1)).getMonsterZone(), Cards.getCardByName(matcher.group(2)));
                 ++done;
             }
         }
         if (done == 0) {
-            for (Cards card : Players.getPlayerByUsername(opponent).getMonsterCardZoneArray()) {
-                Players.getPlayerByUsername(opponent).removeFromArrayList(Players.getPlayerByUsername(opponent).getMonsterCardZoneArray(), Players.getPlayerByUsername(opponent).getMonsterZone(), cards);
+            for (Cards card : Players.getPlayerByUsername(matcher.group(1)).getMonsterCardZoneArray()) {
+                Players.getPlayerByUsername(matcher.group(1)).removeFromArrayList(Players.getPlayerByUsername(matcher.group(1)).getMonsterCardZoneArray(), Players.getPlayerByUsername(matcher.group(1)).getMonsterZone(), Cards.getCardByName(matcher.group(2)));
                 return;
             }
         }
     }
 
-    public void solemnWarning(String turn) {
-        Players.getPlayerByUsername(turn).decreaseLifePoint(2000);
+    public static void solemnWarning(Matcher turn) {
+        Players.getPlayerByUsername(turn.group(1)).decreaseLifePoint(2000);
     }
 
-    public void callOfTheHaunted(String turn) {
-        GameProgramController.getInstance().summon(turn);
+    public static void callOfTheHaunted(Matcher turn) {
+        GameProgramController.getInstance().summon(turn.group(1));
     }
 
-    public void commandKnight(String turn) {
-        for (Cards card : Players.getPlayerByUsername(turn).getMonsterCardZoneArray()
+    public static void commandKnight(Matcher turn) {
+        for (Cards card : Players.getPlayerByUsername(turn.group(1)).getMonsterCardZoneArray()
         ) {
-            Players.getPlayerByUsername(turn).increaseATK(card);
+            Players.getPlayerByUsername(turn.group(1)).increaseATK(card);
         }
-        while (Players.getPlayerByUsername(turn).getMonsterCardZoneArray().size() > 1) {
+        while (Players.getPlayerByUsername(turn.group(1)).getMonsterCardZoneArray().size() > 1) {
             Cards.getCardByName("command Knight").setAttackable(false);
         }
     }
 
-    public void yomiShip(String opponent, MonsterCard attackingCard) {
+    public static void yomiShip(Matcher matcher) {//group1 -> Opponent / group2 -> attackingCard
         Cards thisCard = Cards.getCardByName("yomi ship");
-        if (attackingCard.getATK() > thisCard.getDEF()) {
-            Players.getPlayerByUsername(opponent).getMonsterCardZoneArray().remove(attackingCard);
-            Players.getPlayerByUsername(opponent).getAllCardsInHandsArray().remove(attackingCard);
-            Players.getPlayerByUsername(opponent).getCardsInGraveyard().add(attackingCard);
+        if (Cards.getCardByName(matcher.group(2)).getATK() > thisCard.getDEF()) {
+            Players.getPlayerByUsername(matcher.group(1)).getMonsterCardZoneArray().remove(Cards.getCardByName(matcher.group(2)));
+            Players.getPlayerByUsername(matcher.group(1)).getAllCardsInHandsArray().remove(Cards.getCardByName(matcher.group(2)));
+            Players.getPlayerByUsername(matcher.group(1)).getCardsInGraveyard().add(Cards.getCardByName(matcher.group(2)));
         }
     }
 
-    public void suijin(String opponent, MonsterCard attackingCard) {
+    public static void suijin(Matcher attackingCard) {
         int repeat = 0;
-        int originalATK = attackingCard.getATK();
+        int originalATK = MonsterCard.getCardByName(attackingCard.group(1)).getATK();
         if (repeat == 0) {
 
-            attackingCard.setATK(0);
+            MonsterCard.getCardByName(attackingCard.group(1)).setATK(0);
             repeat += 1;
         }
-        attackingCard.setATK(originalATK);
+        MonsterCard.getCardByName(attackingCard.group(1)).setATK(originalATK);
 
     }
 
-    public void crabTurtle() {
+    public static void crabTurtle(Matcher matcher) {
         Cards.getCardByName("crab Turtle").canRitualSummon();
     }
 
-    public void skullGuardian() {
+    public static void skullGuardian(Matcher matcher) {
         Cards.getCardByName("skull Guardian").canRitualSummon();
     }
 
-    public void manEaterBug(String opponent, Cards card) {
-        Players.getPlayerByUsername(opponent).getMonsterCardZoneArray().remove(card);
+    public static void manEaterBug(Matcher matcher) {      //group1 -> Opponent / group2 -> Card;
+        Players.getPlayerByUsername(matcher.group(1)).getMonsterCardZoneArray().remove(Cards.getCardByName(matcher.group(2)));
     }
 
-    public void gateGuardian(String turn, ArrayList<MonsterCard> toBeSacirfised) {
-        for (MonsterCard card : toBeSacirfised
-        ) {
-            Players.getPlayerByUsername(turn).getMonsterCardZoneArray().remove(card);
+    public static void gateGuardian(Matcher matcher) {//group1 -> Turn
+        for (Cards card : MonsterCard.allCards) {
+            Players.getPlayerByUsername(matcher.group(1)).getMonsterCardZoneArray().remove(card);
         }
         Cards.getCardByName("Gate Guardian").setCanSummon(true);
     }
 
-    public void scannerCard(String turn, String opponent, Cards cards) {
-        Players.getPlayerByUsername(turn).putInMonsterZone(cards, cards.getCardName());
+    public static void scannerCard(Matcher matcher) {//group1 -> Turn / gropu2 -> Card
+        Players.getPlayerByUsername(matcher.group(1)).putInMonsterZone(Cards.getCardByName(matcher.group(2)), Cards.getCardByName(matcher.group(2)).getCardName());
     }
 
 }

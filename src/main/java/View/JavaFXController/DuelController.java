6ef0fdd0393578f3efc70e4cf3979ee55cbfb2df
HamidReza <hr.m.szh@gmail.com>
2021-07-03 12:12:38 +0430

    Complete ScoreBoard

--- src/main/java/View/JavaFXController/DuelController.java
+++ src/main/java/View/JavaFXController/DuelController.java
@@ -1,4 +1,22 @@
 package View.JavaFXController;
 
-public class DuelController {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+
+public class DuelController extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL duel = getClass().getResource("/fxml/DuelMenu.fxml");
+        Parent root = FXMLLoader.load(duel);
+        Scene scene = new Scene(root);
+        root.setId("duelMenu");
+        scene.getStylesheets().addAll(this.getClass().getResource("/css/style.css").toExternalForm());
+        stage.setScene(scene);
+    }
 }
--- src/main/java/View/JavaFXController/ShopController.java
+++ src/main/java/View/JavaFXController/ShopController.java
@@ -1,4 +1,21 @@
 package View.JavaFXController;
 
-public class ShopController {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.net.URL;
+
+public class ShopController extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL shop = getClass().getResource("/fxml/ShopMenu");
+        Parent root = FXMLLoader.load(shop);
+        Scene scene = new Scene(root);
+        root.setId("shop");
+        scene.getStylesheets().addAll(this.getClass().getResource("/css/style.css").toExternalForm());
+        stage.setScene(scene);
+    }
 }

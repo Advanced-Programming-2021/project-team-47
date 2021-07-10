eb893943d0a718b3782a94a3e07dc277d425d609
USER <eshagh.abolfazl@gmail.com>
2021-07-10 22:41:24 +0430

    I fucking hate graphics , profileController edition 1

--- src/main/java/View/ProfileController.java
+++ src/main/java/View/ProfileController.java
@@ -1,14 +1,36 @@
 package View;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.net.URL;
 
 public class ProfileController extends Application {
+    @FXML
+    private ImageView profileImageView;
+    @FXML
+    private Label usernameLabel;
+    @FXML
+    private TextField aliasTextField;
+    @FXML
+    private Button passwordChangeButton;
+    @FXML
+    private Button profilePhotoChange;
+    public void changePassword(ActionEvent event){
+
+    }
+    public void changePhoto(ActionEvent event){
+
+    }
     public static ProfileController profileController;
     public static ProfileController getInstance() {
         if (profileController == null) profileController = new ProfileController();
